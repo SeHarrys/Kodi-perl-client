20d26f12d5f9510896b755b9645d44cb188e6319
H <trycky@gmail.com>
2014-11-17 00:35:13 +0100

    Changes

--- Kodi.pl
+++ Kodi.pl
@@ -1,4 +1,12 @@
 #!/usr/bin/env perl
+=doc
+ Generate API: Kodi/Methods.pm
+ ./Kodi.pl gen
+
+ ./Kodi.pl Application.SetVolume '{"volume":10}'
+ ./Kodi.pl PlayerPlayPause '{"playerid":1}'
+
+=cut
 
 use Kodi;
 use JSON qw(decode_json);
@@ -9,25 +17,21 @@ exit unless $ARGV[0];
 
 my $X = Kodi->new('192.168.1.107:8080');
 
-## Generate API Methods
 if ( $ARGV[0] eq 'gen' ) {
     $X->GenMethods();
     exit;
 }
 
-#$X->PlayerPlayPause( { playerid => 1 });
-#$X->GUIShowNotification( { title => 'Oh yeah' , message => 'Doble yeahhhh' });
-#$X->PlayerGetActivePlayers();
-#$X->ApplicationSetVolume( { volume => 89 } );
-#$X->VideoLibraryGetMovies();
-
-# ./Kodi.pl PlayerGetActivePlayers '{"volume":10}'
-# ./Kodi.pl PlayerPlayPause '{"playerid":1}'
-
 my $nam = $ARGV[0];
+$nam =~ s/\.//;
 
 if ( $ARGV[1]) {
     $X->$nam(decode_json($ARGV[1]));
 } else {
     $X->$nam();
 }
+
+#$X->PlayerPlayPause( { playerid => 1 });
+#$X->GUIShowNotification( { title => 'Oh yeah' , message => 'Doble yeahhhh' });
+#$X->PlayerGetActivePlayers();
+#$X->ApplicationSetVolume( { volume => 89 } );
